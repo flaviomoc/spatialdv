15fe72ba2002c67ba5f88c577750585081aea6e0
Flavio Mota <flaviomoc@gmail.com>
2022-06-25 23:31:29 -0300

    Function added and documented
    License added
    
    Description updated

---
+++ R/spatialdv.R
@@ -0,0 +1,64 @@
+#' Calculate diversity patterns for spatial data
+#'
+#' Calculate alpha and beta diversity for spatial data
+#'
+#' @param x Object of class SpatRaster with binarized distribution projected to all species in the reference scenario
+#' @param y Object of class SpatRaster with binarized distribution projected to all species in the future scenario
+#' @param z Index to be return
+#'
+#' @return Plot object of class SpatRaster according to the index chosen
+#' @export
+#'
+#' @examples
+#' require(terra)
+#' set.seed(100)
+#' ref <- rast(array(sample(c(rep(1, 750), rep(0, 250))), dim = c(10, 10, 10)))
+#' fut <- rast(array(sample(c(rep(1, 300), rep(0, 700))), dim = c(10, 10, 10)))
+#' spatialdv(ref, fut, "beta")
+#'
+spatialdv <- function(x, y, z = "delta"){
+  if(z == "alpha1"){
+    res <- sum(x > 0) # richness of reference scenario
+    names(res) <- "Baseline richness"
+  } else if(z == "alpha2"){
+    res <- sum(y > 0) # richness of future scenario
+    names(res) <- "Future richness"
+  } else if(z == "delta"){
+    res <- sum(y > 0) - sum(x > 0) # difference between future and reference scenarios
+    names(res) <- "Delta richness"
+  } else if(z == "beta"){
+    total <- x + y # total number of species
+    a <- sum(total == 2) # species in both scenarios simultaneously
+    b <- sum(x > y, 1, 0) # species in reference scenario only
+    c <- sum(y > x, 1, 0) # species in future scenario only
+    res <- (b + c) / (a + b + c) # beta diversity calculation
+    names(res) <- "Beta diversity"
+  } else if(z == "turn"){
+    total <- x + y
+    a <- sum(total == 2)
+    b <- sum(x > y, 1, 0)
+    c <- sum(y > x, 1, 0)
+    Bjac <- (b + c) / (a + b + c)
+    res <- (2 * min(b, c)) / (a + (2 * min(b, c))) # turnover calculation
+    names(res) <- "Turnover"
+  } else if(z == "nest"){
+    total <- x + y
+    a <- sum(total == 2)
+    b <- sum(x > y, 1, 0)
+    c <- sum(y > x, 1, 0)
+    Bjac <- (b + c) / (a + b + c)
+    Bjtu <- (2 * min(b, c)) / (a + (2 * min(b, c)))
+    res <- Bjac - Bjtu # nestedness calculation
+    names(res) <- "Nestedness"
+  } else if(z == "ratio"){
+    total <- x + y
+    a <- sum(total == 2)
+    b <- sum(x > y, 1, 0)
+    c <- sum(y > x, 1, 0)
+    Bjac <- (b + c) / (a + b + c)
+    Bjtu <- (2 * min(b, c)) / (a + (2 * min(b, c)))
+    res <- Bjtu / Bjac # ratio calculation (turnover divided by beta diversity)
+    names(res) <- "Turnover/Beta total"
+  }
+  return(terra::plot(res, main = names(res))) # plot SpatRaster calculated for chosen index
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+
+# spatialdv
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of spatialdv is to calculate diversity patterns for spatial data
+
+Function added and documented
+
+License added
+
+Description updated
+
+## Installation
+
+You can install the development version of spatialdv from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("flaviomoc/spatialdv")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(spatialdv)
+## basic example code
+```
+
