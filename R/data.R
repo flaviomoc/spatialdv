8dceee503c2a144de0fc3e141b5f6e08b750d4ec
Flavio Mota <flaviomoc@gmail.com>
2022-06-27 09:07:05 -0300

    Description of external data updated

--- R/data.R
+++ R/data.R
@@ -1,6 +1,8 @@
-#' SpatRaster objects
+#' SpatRaster and data.frame objects
 #'
-#' Random dataset containing binary maps of 10 species for reference and future scenarios
-#' @format SpatRaster created from random array. 10 cols, 10 rows, and 10 layers
+#' *ref.tif* is an object of class SpatRaster created from a random array representing binary maps of 10 species in the reference scenario.
+#' *fut.tif* is an object of class SpatRaster created from a random array representing binary maps of 10 species in the future scenario.
+#' *traits.xlsx* is an object of class data.frame created from random vectors representing species traits: body mass and elevation.
+#' @format SpatRaster: 10 columns, 10 rows, and 10 layers. Data.frame: 2 columns and 10 rows.
 #' @source Random values
 "data"
