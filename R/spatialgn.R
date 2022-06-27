7991cda7d019571d28ba61b00e73c82bbc465ceb
Flavio Mota <flaviomoc@gmail.com>
2022-06-26 21:52:43 -0300

    New function and external data added

---
+++ R/spatialgn.R
@@ -0,0 +1,26 @@
+#' Calculate average trait generalization for spatial data
+#'
+#' @param x Object of class SpatRaster with binarized distribution projected to all species in the reference scenario
+#' @param y Object of class data.frame with species traits as columns
+#' @param trait Column name to be used
+#'
+#' @return Object of class SpatRaster with average trait chosen
+#' @export
+#'
+#' @examples
+#' set.seed(100)
+#' mass <- runif(10, .5, 2)
+#' elev <- sample(500:1000, 10)
+#' ref <- terra::rast(array(sample(c(rep(1, 750), rep(0, 250))), dim = c(10, 10, 10)))
+#' traits <- as.data.frame(cbind(mass, elev))
+#' rownames(traits) <- paste0("sp", 1:nrow(traits))
+#' spatialgen(ref, traits, "mass")
+#' spatialgen(ref, traits, "elev")
+#'
+spatialgen <- function(x, y, trait) {
+  t <- y[ , trait]
+  gen <- x * t
+  res <- terra::app(gen, mean)
+  names(res) <- trait
+  return(terra::plot(res, main = names(res)))
+}
--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -5,9 +5,16 @@ ref <- terra::rast(array(sample(c(rep(1, 750), rep(0, 250))), dim = c(10, 10, 10
 
 fut <- terra::rast(array(sample(c(rep(1, 300), rep(0, 700))), dim = c(10, 10, 10)))
 
+mass <- runif(10, .5, 2)
+elev <- sample(500:1000, 10)
+traits <- as.data.frame(cbind(mass, elev))
+rownames(traits) <- paste0("sp", 1:nrow(traits))
+
 usethis::use_data(data, overwrite = TRUE)
 
 # exporting SpatRaster
 if(!dir.exists("inst/extdata")) dir.create("inst/extdata", recursive = TRUE)
 terra::writeRaster(ref, "inst/extdata/ref.tif")
 terra::writeRaster(fut, "inst/extdata/fut.tif")
+
+writexl::write_xlsx(traits, "data-raw/traits.xlsx")
